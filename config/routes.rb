c42f273885c8534b4ca6d64deda148ecf081fe51
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2023-02-08 08:15:45 +0000

    UserとAdminのログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions'
+}
+  get 'homes/top'
+  root to: "homes#top"
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
