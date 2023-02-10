261db41dd69271faf98bc9ac101d1912ec55b0c8
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2023-02-10 14:15:05 +0000

    いいね機能非同期修正必要

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,17 @@
 class FavoritesController < ApplicationController
     
 def create
-  @recipe_favorite = Favorite.new(user_id: current_user.id, recipe_id: params[:recipe_id])
-  @recipe_favorite.save
+  @recipe = Recipe.find(params[:recipe_id])
+  recipe_favorite = Favorite.new(user_id: current_user.id, recipe_id: params[:recipe_id])
+  recipe_favorite.save
   redirect_to recipe_path(params[:recipe_id]) 
+  
 end
 
 def destroy
-  @recipe_favorite = Favorite.find_by(user_id: current_user.id, recipe_id: params[:recipe_id])
-  @recipe_favorite.destroy
+  @recipe = Recipe.find(params[:recipe_id])
+  recipe_favorite = Favorite.find_by(user_id: current_user.id, recipe_id: params[:recipe_id])
+  recipe_favorite.destroy
   redirect_to recipe_path(params[:recipe_id]) 
 end
     
