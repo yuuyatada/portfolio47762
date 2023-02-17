123fbfcffbacc5ca841ed33e93b6246748f76c59
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2023-02-17 09:10:26 +0000

    レイアウト調整、css不具合

--- Gemfile
+++ Gemfile
@@ -56,4 +56,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'jquery-rails'
+gem 'bootstrap', '~> 4.3.1'
 gem 'image_processing', '~> 1.2'
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,10 +9,14 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
 import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application" 
+import '@fortawesome/fontawesome-free/js/all';
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
