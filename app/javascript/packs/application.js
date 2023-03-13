de74dcbe9037933c96fde43b9775faf691e4bba4
EC2 Default User <c134141@gmail.com>
2023-03-13 10:29:32 +0000

    TOPページへ移動する際のアニメーションの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,5 +16,9 @@ Turbolinks.start()
 ActiveStorage.start()
 require('./preview') 
 
+$(window).on('load',function(){
+  $("#loading").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#loading_box").delay(1200).fadeOut('slow');//ローディングテキストを1.2秒（1200ms）待機してからフェードアウト
+});
 
 
