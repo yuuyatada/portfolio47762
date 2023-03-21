6a1e8f565596c402c22fbc788df63584abb88928
EC2 Default User <c134141@gmail.com>
2023-03-21 11:06:05 +0000

    文字数制限の表示と超過した場合の表示の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,4 +21,24 @@ $(window).on('load',function(){
   $("#loading_box").delay(1200).fadeOut('slow');//ローディングテキストを1.2秒（1200ms）待機してからフェードアウト
 });
 
+$(function (){
+  var count = $(".js-text").text().replace(/\n/g, "改行").length;
+  var now_count = 140 - count;
+  if (count > 140) {
+    $(".js-text-count").css("color","red");
+  }
+  $(".js-text-count").text( "残り" + now_count + "文字");
+
+  $(".js-text").on("keyup", function() {
+    var count = $(this).val().replace(/\n/g, "改行").length;
+    var now_count = 140 - count;
+    if (count > 140) {
+      $(".js-text-count").css("color","red");
+    } else {
+      $(".js-text-count").css("color","black");
+    }
+    $(".js-text-count").text( "残り" + now_count + "文字");
+  });
+});
+
 
